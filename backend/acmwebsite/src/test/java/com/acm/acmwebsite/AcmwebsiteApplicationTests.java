3df2df1bc2b6f0770a87bc391489713284029d66
noureldeen73 <nourie2003@gmail.com>
2025-10-28 22:03:08 +0300

    Initial Backend Structure

---
+++ backend/acmwebsite/src/test/java/com/acm/acmwebsite/AcmwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acm.acmwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
