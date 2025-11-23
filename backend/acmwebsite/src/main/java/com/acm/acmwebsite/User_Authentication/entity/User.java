d398bf3a49c18ed7dd591b2ca9a52be6cb8ed6df
Omar Abdelmawgoud <Omar.Abdelmawgoud10@gmail.com>
2025-11-23 20:55:58 +0200

    ACM-869az5ea1 | Create User Register API
    added some unit tests

--- backend/acmwebsite/src/main/java/com/acm/acmwebsite/User_Authentication/entity/User.java
+++ backend/acmwebsite/src/main/java/com/acm/acmwebsite/User_Authentication/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
