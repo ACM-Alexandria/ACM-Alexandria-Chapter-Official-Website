e8d44fceac16f6ffd58a615b75f476b810607ba5
Omar Abdelmawgoud <Omar.Abdelmawgoud10@gmail.com>
2025-11-25 21:35:25 +0200

    ACM-869b6ck5h | Create RefreshToken model
    finished creating the model of refresh token with all the validations and indexes required

---
+++ backend/acmwebsite/src/main/java/com/acm/acmwebsite/User_Authentication/repository/RefreshTokenRepository.java
@@ -0,0 +1,8 @@
+package com.acm.acmwebsite.User_Authentication.repository;
+
+import com.acm.acmwebsite.User_Authentication.entity.RefreshToken;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/** RefreshTokenRepository */
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {}
