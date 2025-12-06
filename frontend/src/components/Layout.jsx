16e2ec2abb52b397d313c0f69d9e5ed4ad002210
noureldeen73 <nourie2003@gmail.com>
2025-12-06 23:04:44 +0200

    Added Footer

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Footer } from "./Footer.jsx";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
