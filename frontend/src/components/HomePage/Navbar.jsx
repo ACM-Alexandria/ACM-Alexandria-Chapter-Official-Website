be3a987abbe97253518a38cf82da3c9745c1e23c
omarzydan610 <omarzydan610@gmail.com>
2026-02-10 22:05:59 +0200

    refactor navbar

--- frontend/src/components/HomePage/Navbar.jsx
+++ frontend/src/components/HomePage/Navbar.jsx
@@ -1,54 +1,29 @@
-import logo from "../../assets/acm-logo.png";
+import logo from "../../assets/logo/acm-logo-no-bg.png";
 
 const Navbar = () => {
-  return (
-    <nav className="w-full h-[70px] bg-[#205E85] flex items-center justify-between px-10 text-white fixed top-0 z-10">
-      <div className="flex items-center gap-10">
-        <img src={logo} className="h-10" alt="ACM Logo" />
+  const navLinkClass =
+    "no-underline text-white text-[22px] font-semibold hover:opacity-80";
+  const navItems = [
+    { href: "#about", label: "About Us" },
+    { href: "#services", label: "Services" },
+    { href: "#programs", label: "Programs" },
+    { href: "#events", label: "Events" },
+    { href: "#clubs", label: "Clubs" },
+  ];
 
-        <ul className="list-none flex gap-6">
-          <li>
-            <a
-              href="#about"
-              className="no-underline text-white text-[15px] hover:opacity-80"
-            >
-              About Us
+  return (
+    <nav className="w-full h-[70px] bg-gradient-to-r from-[#4B98C8] to-[#205E85] flex items-center justify-between px-10 text-white fixed top-0 z-10 gap-12">
+      <img src={logo} className="h-20" alt="ACM Logo" />
+
+      <ul className="list-none flex gap-12 ml-auto">
+        {navItems.map((item) => (
+          <li key={item.href}>
+            <a href={item.href} className={navLinkClass}>
+              {item.label}
             </a>
           </li>
-          <li>
-            <a
-              href="#clubs"
-              className="no-underline text-white text-[15px] hover:opacity-80"
-            >
-              Clubs
-            </a>
-          </li>
-          <li>
-            <a
-              href="#events"
-              className="no-underline text-white text-[15px] hover:opacity-80"
-            >
-              Events
-            </a>
-          </li>
-          <li>
-            <a
-              href="#programs"
-              className="no-underline text-white text-[15px] hover:opacity-80"
-            >
-              Programs
-            </a>
-          </li>
-          <li>
-            <a
-              href="#services"
-              className="no-underline text-white text-[15px] hover:opacity-80"
-            >
-              Services
-            </a>
-          </li>
-        </ul>
-      </div>
+        ))}
+      </ul>
 
       <a
         className="bg-white text-[#2c4a72] py-2 px-[18px] rounded-md no-underline font-bold hover:bg-gray-200"
