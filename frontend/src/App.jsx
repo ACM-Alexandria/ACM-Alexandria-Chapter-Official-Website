4619fd2d7f581e742d9fbe0ea0a641707edd3005
Omar Abdelmawgoud <Omar.Abdelmawgoud10@gmail.com>
2026-02-12 20:27:53 +0200

    ACM-869az5g7u | New Password Form
    merged frontend structure and implemented reset password page which was divided on page folder and services folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import ResetPassword from "./pages/ResetPassword";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
